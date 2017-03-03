5fea8764ed28371e0e8b16a3ef3a21e9fe3504f4
Robert Gebauer <RobertGebauer@users.noreply.github.com>
2017-03-03 11:35:19 +0100

    Load config relative to current dir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,13 @@
  **  Copyright (c) 2016 msg systems ag (http://www.msg.group/)
  */
 
+var path = require("path");
+
 module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-connect')
     grunt.loadNpmTasks('grunt-connect-proxy')
     grunt.loadNpmTasks('grunt-extend-config')
 
-    grunt.loadTasks("grunt-config")
-
+    grunt.loadTasks(path.join(__dirname, "grunt-config"))
 };
